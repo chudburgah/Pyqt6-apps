5e38075bed08936e9e4c168c55d52b8ba88c16d3
Lucas Redham2 <chudburgah@gmail.com>
2026-08-03 16:30:25 +1000

    the fricking year thing isnt working

--- Mp3Tagger/main.py
+++ Mp3Tagger/main.py
@@ -2,7 +2,7 @@ from PyQt6.QtWidgets import *; from PyQt6.QtCore import *; from PyQt6.QtGui impo
 from pathlib import Path
 import sys
 import eyed3
-import logging
+from eyed3.core import Date
 
 script_dir = Path(__file__).resolve().parent
 
@@ -20,12 +20,23 @@ class MainWindow(QMainWindow):
         self.Log_hint = QLabel()
         self.Log_hint.setWordWrap(True)
         self.Update_propertys_button = QPushButton("Update propertys")
+        self.label = QLabel("Update:")
+        self.label.alignment()
+        self.Type_selection = QComboBox()
+        self.Type_selection.addItems(["Album", "Artist", "Year"])
         
         self.Open_button.clicked.connect(self.pressed)
         self.Update_propertys_button.clicked.connect(self.Update_file)
+        self.Type_selection.currentIndexChanged.connect(self.Type_changed)
         
         layout1 = QVBoxLayout()
+        layout2 = QHBoxLayout()
+        
+        layout2.addWidget(self.label)
+        layout2.addWidget(self.Type_selection)
+        
         layout1.addWidget(self.Open_button)
+        layout1.addLayout(layout2)
         layout1.addWidget(self.New_album_input)
         layout1.addWidget(self.Log_hint)
         layout1.addWidget(self.Update_propertys_button)
@@ -40,21 +51,32 @@ class MainWindow(QMainWindow):
         if file_path:
             for i in range(len(file_path)):
                 print(file_path[int(i)])
+                
     def Update_file(self):
         try:
             for i in range(len(file_path)):
                 audiofile = eyed3.load(file_path[i])
                 if audiofile.tag is None:
                     audiofile.initTag()
+                    
+                print(self.Type_selection.currentIndex())
+                if self.Type_selection.currentIndex() == 0:
+                    audiofile.tag.album = self.New_album_input.text()
+                elif self.Type_selection.currentIndex() == 1:
+                    audiofile.tag.artist = self.New_album_input.text()
+                elif self.Type_selection.currentIndex() == 2:
+                    audiofile.tag.
+                    audiofile.tag.original_release_date = Date(2026)
+                    
                 
-                audiofile.tag.album = self.New_album_input.text()
-                
-                audiofile.tag.save()
+                audiofile.tag.save(version=eyed3.id3.ID3_DEFAULT_VERSION)
                 self.Log_hint.setText(f"Upated album name for {len(file_path)} song(s)")
         except:
             self.Log_hint.setText("failed to update propertys")
-
-        
+            
+    def Type_changed(self, i):
+        print(i)
+  
 app = QApplication(sys.argv)
 
 window = MainWindow()
