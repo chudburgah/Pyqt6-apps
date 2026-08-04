719b755ef670dcb82200f6106d64ea1705040aa5
Lucas Redham2 <chudburgah@gmail.com>
2026-08-04 10:13:27 +1000

    add genre option to Type selection and update logic for genre tagging

--- Mp3Tagger/Mp3 Tagger.py
+++ Mp3Tagger/Mp3 Tagger.py
@@ -22,7 +22,7 @@ class MainWindow(QMainWindow):
         self.Update_propertys_button = QPushButton("Update propertys")
         self.label = QLabel("Update:")
         self.Type_selection = QComboBox()
-        self.Type_selection.addItems(["Album", "Artist", "Year"])
+        self.Type_selection.addItems(["Album", "Artist", "Year", "Genre"])
         
         self.Open_button.clicked.connect(self.pressed)
         self.Update_propertys_button.clicked.connect(self.Update_file)
@@ -55,7 +55,6 @@ class MainWindow(QMainWindow):
             for i in range(len(file_path)):
                 audio = file_path[i]
                 audiofile = EasyID3(audio)
-                tags = ID3(audio)
                     
                 print(self.Type_selection.currentIndex())
                 if self.Type_selection.currentIndex() == 0:
@@ -63,9 +62,10 @@ class MainWindow(QMainWindow):
                 elif self.Type_selection.currentIndex() == 1:
                     audiofile['artist'] = self.New_album_input.text()
                 elif self.Type_selection.currentIndex() == 2:
-                    audiofile['date'] = self.New_album_input.text()         
+                    audiofile['date'] = self.New_album_input.text()
+                elif self.Type_selection.currentIndex() == 3:
+                    audiofile['genre'] = self.New_album_input.text()
                 
-                tags.save()
                 audiofile.save()
                 self.Log_hint.setText(f"Upated properties for {len(file_path)} song(s)")
         except Exception as e:
