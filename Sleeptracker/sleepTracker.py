743b70ed026527809e502454e117ebb4317fd791
Lucas Redham <chudburgah@gmail.com>
2026-08-18 18:03:09 +1000

    susus

--- Sleeptracker/sleepTracker.py
+++ Sleeptracker/sleepTracker.py
@@ -2,11 +2,27 @@ from PyQt6.QtWidgets import *
 import json
 import datetime
 from pathlib import Path
+import sys
 script_dir = Path(__file__).resolve().parent
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        self.setWindowTitle("Sleep tracker")
-        self.setMinimumSize(200,150)
+        self.setWindowTitle("Sleep Tracker")
+        
+        self.addSleep_b = QPushButton("Press Me!")
+
+        layout1 = QVBoxLayout()
+        
+        layout1.addWidget(self.addSleep_b)
+        
+        contianer = QWidget()
+        contianer.setLayout(layout1)
+        self.setCentralWidget(contianer)
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
